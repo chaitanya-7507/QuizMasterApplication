9d1816593ae60ed661d9db053159e9f1d00d845d
chaitanya-7507 <chaitanya.barappadi07@gmail.com>
2025-11-14 21:26:09 +0530

    Update app.routes.ts

--- quiz-frontend/src/app/app.routes.ts
+++ quiz-frontend/src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { AuthComponent } from './components/auth/auth.component';
 import { AdminDashboardComponent } from './components/dashboard/admin-dashboard/admin-dashboard.component';
 import { ParticipantDashboardComponent } from './components/dashboard/participant-dashboard/participant-dashboard.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: AuthComponent }, // ← FIXED
   { path: 'login', component: AuthComponent },
   { path: 'signup', component: AuthComponent },
   { path: 'admin-dashboard', component: AdminDashboardComponent },
