9e6494e56591d33c57fa1c9e366736a9b987ae71
chaitanya-7507 <chaitanya.barappadi07@gmail.com>
2025-11-14 21:41:12 +0530

    Update app.component.ts

--- quiz-frontend/src/app/app.component.ts
+++ quiz-frontend/src/app/app.component.ts
@@ -1,34 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'quiz-frontend';
-
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    const userData = localStorage.getItem('user');
-
-    // If user exists in localStorage → stay on the same page
-    if (userData) {
-      const user = JSON.parse(userData);
-
-      // OPTIONAL: If user tries to open /login or /signup, redirect them to dashboard
-      const currentUrl = this.router.url;
-
-      if (currentUrl === '/login' || currentUrl === '/signup' || currentUrl === '/') {
-        if (user.role === 'Admin') {
-          this.router.navigate(['/admin-dashboard']);
-        } else if (user.role === 'Participant') {
-          this.router.navigate(['/participant-dashboard']);
-        }
-      }
-    }
-  }
 }
+I have this 
